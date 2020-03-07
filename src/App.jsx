5e1e535702e0524831451cc8aca3aa55c0d88f11
Wouter Vlaeyen <woutvlae@student.arteveldehs.be>
2020-03-07 16:20:37 +0100

    Added actual game

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import VirusGame from './virus-game';
+
+function App() {
+  return (
+    <div className="App">
+      <VirusGame />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/virus-game/config/imageConfig.js
@@ -0,0 +1,14 @@
+/**
+ * All images used in game context
+ * * Please note: All images within PIXI.js have to be .PNG
+ */
+const imageConfig = {
+    viruses: [
+        '/images/virus_monster_1.png',
+        '/images/virus_monster_2.png',
+        '/images/virus_monster_3.png',
+    ],
+    player: '/images/virus.png',
+};
+
+export default imageConfig;
